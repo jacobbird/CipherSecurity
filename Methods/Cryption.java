bb4b2c8c7293c5f6258136c55fab69a667056537
arjun-csekar <32626731+arjun-csekar@users.noreply.github.com>
2017-12-13 22:12:08 -0800

    Update Cryption.java

--- Methods/Cryption.java
+++ Methods/Cryption.java
@@ -2,22 +2,22 @@ package CipherSecurity.Methods;
 
 public class Cryption{
 	
-	int encrypt(char ch, int n, int e)
+	int encrypt(char ch, int n, int e)//Encryptes by using the formula ch^e % n. As thats a large number we put it in loop
 	{
 		int i;
 		int temp = ch;
 		for (i = 1; i < e; i++)
 			temp = (temp * ch) % n;
-		return temp;
+		return temp; //Returns integer
 	}
 
-	char decrypt(int ch, int n, int d)
+	char decrypt(int ch, int n, int d) //Decryptes by using the formula ch^d % n. As thats a large number we put it in loop
 	{
 		int i;
 		int temp = ch;
 		for (i = 1; i < d; i++)
 			ch = (temp * ch) % n;
-		return (char)(ch);
+		return (char)(ch); //Returns character
 	}
 
 }
